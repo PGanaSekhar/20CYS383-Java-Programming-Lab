523bbd2d60c40b6963de6262e719a029e01428cc
PGanaSekhar <101617845+PGanaSekhar@users.noreply.github.com>
2023-05-31 23:21:07 +0530

    Arrays, ArrayLists, HashMap, LinkedList

---
+++ Records/21CYS/CB.EN.U4CYS21055/datastructures/Array.java
@@ -0,0 +1,44 @@
+package com.ganasekhar.amrita.cys.jpl.datastructures;
+
+import javax.lang.model.element.Name;
+
+/**
+ * The ArrayExample class demonstrates the usage of an array to store and display roll numbers.
+ * It creates an array of roll numbers and prints each roll number using a for loop.
+ * This class provides a basic example of working with arrays in Java.
+ *
+ * Usage:
+ * - The program creates an array of roll numbers with a specified size.
+ * - It assigns roll numbers to array elements.
+ * - It uses a for loop to iterate through the array and print each roll number.
+ *
+ * Note: This example assumes a fixed size for the array and assigns values manually.
+ *
+ * Dependencies:
+ * - None
+ *
+ * @author Penugonda V S Ganasekhar
+ * @version 0.5
+ */
+public class Array{
+    /**
+     * The main method is the entry point of the program.
+     * It creates an array of roll numbers and prints each roll number using a for loop.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String args[]) {
+        String Names[] = new String[5];
+
+        Names[0] = "Hamilton";
+        Names[1] = "George";
+        Names[2] = "Alice";
+        Names[3] = "Bob";
+        Names[4] = "Henry";
+
+        for (int i = 0; i < Names.length; i++) {
+            System.out.println(Names[i]);
+        }
+    }
+}
+
